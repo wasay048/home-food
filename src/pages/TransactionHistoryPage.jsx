ddfe9fd9da0e402895f46e885224ac6c04b4fb8a
Abdul Wasay <wasayhayee@gmail.com>
2026-06-10 18:31:45 +0500

    feat: add Redux selector for user state in TransactionHistoryPage

--- src/pages/TransactionHistoryPage.jsx
+++ src/pages/TransactionHistoryPage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useCallback, useRef } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import {
   collection,
